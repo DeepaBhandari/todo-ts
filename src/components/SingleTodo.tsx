0727dfe06a495a85df674d16492a700b71c5f706
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-01-23 13:08:46 +0545

    fix css of todos

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Todo } from './modal'
+import {AiFillEdit,AiFillDelete} from 'react-icons/ai'
+import {MdDone} from 'react-icons/md'
+import "./styles.css";
+
 type Props = {
  todo: Todo,
  todos: Todo[],
@@ -12,6 +16,11 @@ const SingleTodo= ({todo, todos, setTodos}: Props) => {
       <span className="todos__single--text">
         {todo.todo}
       </span>
+      <div>
+        <span className="icon"><AiFillEdit/></span>
+        <span className="icon"><AiFillDelete/></span>
+        <span className="icon"><MdDone/></span>
+      </div>
     </form>
   )
 }
