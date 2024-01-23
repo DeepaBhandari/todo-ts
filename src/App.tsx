6e3ac3d19db244aa39229204261c9dd3cf555071
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-01-23 12:37:26 +0545

    addition of todos below input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './components/modal';
+import TodoList from './components/TodoList';
 
 const App :React.FC= () => {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) =>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo: todo, isDone: false}]);
+      setTodo("");
+    }
+  };
+  console.log(todo);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo ={todo} setTodo ={setTodo} handleAdd ={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
+      {todos.map((t)=>(
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css';
 
-const InputField = () => {
-  return (
-    <form className ='input'>
-        <input type="input" placeholder = "Enter a task" className ="input__box"/>
+interface Props{
+    todo:string;
+    setTodo:  React.Dispatch<React.SetStateAction<string>>
+    handleAdd : (e: React.FormEvent)=> void;
+
+}
+const InputField:React.FC<Props> = ({todo,setTodo, handleAdd}) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+    return (
+    <form className ="input" onSubmit={(e)=>{handleAdd(e)
+    inputRef.current?.blur();
+    }} >
+        <input type="input" placeholder = "Enter a task" className ="input__box"
+        ref = {inputRef}
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
+        />
    <button className="input_submit" type ="submit">
      Go
    </button>
