78a9c38d8c6cf0014b15ab5d1af1cccd74622f6c
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-01-23 14:23:52 +0545

    react-beautiful-dnd addition

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './components/modal';
 import TodoList from './components/TodoList';
+import { DragDropContext } from "react-beautiful-dnd"
 
 const App :React.FC= () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
-
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
   const handleAdd = (e: React.FormEvent) =>{
     e.preventDefault();
     if(todo){
@@ -17,11 +18,16 @@ const App :React.FC= () => {
   };
   console.log(todo);
   return (
+    <DragDropContext onDragEnd={()=>{}}>
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo ={todo} setTodo ={setTodo} handleAdd ={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <TodoList todos={todos} setTodos={setTodos}
+      completedTodos={completedTodos}
+      setCompletedTodos = {setCompletedTodos}
+      />
     </div>
+    </DragDropContext>
   );
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,27 +2,37 @@ import React from 'react'
 import "./styles.css"
 import { Todo } from './modal';
 import SingleTodo from './SingleTodo';
+import { Droppable } from 'react-beautiful-dnd';
 
 interface Props{
     todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    completedTodos: Todo[]
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
-const TodoList: React.FC<Props> = ({todos,setTodos}) => {
+
+const TodoList: React.FC<Props> = ({todos,setTodos,completedTodos,setCompletedTodos}) => {
   return (
     <div className="container">
-    <div className="todos">
-        <span className="todos__heading">
-            Active Tasks
-        </span>
-        { todos.map(todo=>(
-           <SingleTodo todo ={todo} key={todo.id} todos={todos}
-           setTodos={setTodos}
-           />
-        ))
-        }
-        
-        </div>
+        <Droppable droppableId='TodosList'>
+            {
+                (provided)=>(
+                    <div className="todos" ref={provided.innerRef} {...provided.droppableProps}>
+                    <span className="todos__heading">
+                        Active Tasks
+                    </span>
+                    { todos.map(todo=>(
+                       <SingleTodo todo ={todo} key={todo.id} todos={todos}
+                       setTodos={setTodos}
+                       />
+                    ))
+                    }
+                    
+                    </div>
+                )
+            }
+   
+        </Droppable>
         <div className="todos remove">
         <span className="todos__heading">
             Completed Tasks
