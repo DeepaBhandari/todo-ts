1c3690b30a69b92417b670acc0b26b41d50ee2bf
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-01-23 13:53:43 +0545

    simple todo with edit , delete and done functionality addition

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -3,3 +3,34 @@ export interface Todo{
     todo:string;
     isDone: boolean;
 }
+// type Actions =|{
+//     type:'add',
+//     payload: string
+// }|{
+//     type:'remove',
+//     payload: number
+// }|
+// {
+//     type:'done',
+//     payload: number
+// }
+// import React,{useReducer} from 'react'
+// const TodoReducer = (state: Todo[],action)=>{
+//    switch(action.type){
+//     case "add":
+//         return[
+//             ...state,
+//             {id: Date.now(), todo: action.payload, isDone: false}
+//         ];
+//         case "remove":
+//             return state.filter((todo)=> todo.id !== action.payload)
+//    }
+// }
+// const ReducerExample = () => {
+//     const [state, dispatch] useReducer(TodoReducer, [])
+//   return (
+//     <div>modal</div>
+//   )
+// }
+
+// export default modal
